2ba1ccc472659c509ae9ce17198dc9bf165de785
BiancaCastanheira <biancacostacastanheira@gmail.com>
2025-04-07 21:04:38 -0300

    💄 Adds customize fonts with next/fonts + lint

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+// Link to Google fonts: https://fonts.google.com
+
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
